5e0cb7829d57566afc2b81cfd98a7ad5475b73f3
PierreCointre <pierre.cointre37@gmail.com>
2025-12-04 12:24:57 +0100

    J'ai ajouté des trucs

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
-#include <stdio.h>
-#include <SDL2/SDL.h>
+#include "memory.h"
+#include "misc/debug.h"
 
-int main(int argc, char* argv[]) {
-    // 1. Initialisation de SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        // En cas d'échec de l'initialisation (très rare après l'installation)
-        fprintf(stderr, "Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
+
+int main(int argc, char** argv) {
+    struct memory mem = {0};
+
+    int bytes_loaded = memory_load_rom(&mem, "1-chip8-logo.ch8", START_ADDRESS);
+    if (bytes_loaded < 0) {
+        printf("Erreur lors du chargement de la ROM\n");
         return 1;
     }
 
-    // 2. Création d'une fenêtre simple (640x480 pixels)
-    SDL_Window* window = SDL_CreateWindow("Test SDL2 CHIP-8",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          640, 480,
-                                          SDL_WINDOW_SHOWN);
-
-    if (window == NULL) {
-        // En cas d'échec de la création de fenêtre
-        fprintf(stderr, "Erreur lors de la création de la fenêtre : %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
-    printf("SDL2 est fonctionnel ! La fenêtre va se fermer dans 3 secondes.\n");
-
-    // 3. Pause pour que vous puissiez voir la fenêtre
-    SDL_Delay(3000); // 3000 millisecondes = 3 secondes
-
-    // 4. Nettoyage
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    uint16_t instruction = memory_read_instruction(&mem, START_ADDRESS );
+    printf("Instruction à 0x%X : 0x%X\n", START_ADDRESS, instruction);
+    printf(instruction_as_str(instruction));
     return 0;
 }
+
